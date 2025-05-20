2b450b6fd8880ab845a332f9447a636b6dcfcf61
Oliwia <wardulinskao@gmail.com>
2025-05-20 18:56:00 +0200

    feat: add header

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.getElementById("mobile-menu-open");
+  const closeButton = document.getElementById("mobile-menu-close");
+  const header = document.getElementById("mobile-menu");
+
+  function toggleMenu() {
+    const isVisible = header.classList.toggle("header__mobile--visible");
+    hamburger.setAttribute("aria-expanded", isVisible.toString());
+  }
+
+  hamburger.addEventListener("click", toggleMenu);
+  closeButton.addEventListener("click", toggleMenu);
+});
