0a6f1e1de84836c1e91e8bc0cc2b05845979d28e
Oliwia <wardulinskao@gmail.com>
2025-05-21 21:50:31 +0200

    feat: add faqs

--- src/script.js
+++ src/script.js
@@ -1,8 +1,10 @@
 import "./sass/main.scss";
 import { initMobileMenu } from "./js/mobileMenu";
 import { initRegisterForm } from "./js/register";
+import { initFaq } from "./js/faq";
 
 document.addEventListener("DOMContentLoaded", () => {
   initMobileMenu();
   initRegisterForm();
+  initFaq();
 });
